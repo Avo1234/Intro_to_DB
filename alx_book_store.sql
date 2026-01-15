abe988802c07a536008d879747be2cefc4f47f9d
Herbert Asante <herbertasante66@gmail.com>
2026-01-15 14:07:19 +0000

    Alx book store DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,8 +33,10 @@ order_date DATE
 
 CREATE TABLE Order_Details (
 orderdetailid INT PRIMARY KEY,
-order_id INT, FOREIGN KEY (order_id) REFERENCES  Orders(order_id),
-book_id INT, FOREIGN KEY (book_id) REFERENCES  Books(book_id),
+order_id INT,
+FOREIGN KEY (order_id) REFERENCES  Orders(order_id),
+book_id INT,
+FOREIGN KEY (book_id) REFERENCES  Books(book_id),
 
 order_date DATE
 );
