899200256289f8cfb4151301adcacde2d5f2b0b0
Herbert Asante <herbertasante66@gmail.com>
2026-01-15 14:13:30 +0000

    Alx book store DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,18 +28,21 @@ address TEXT
 CREATE TABLE Orders (
 order_id INT PRIMARY KEY,
 customer_id INT,
-FOREIGN KEY (customer_id) REFERENCES  Customers(customer_id),
-order_date DATE
+order_date DATE,
+
+FOREIGN KEY (customer_id) REFERENCES  Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
 orderdetailid INT PRIMARY KEY,
 order_id INT,
-FOREIGN KEY (order_id) REFERENCES  Orders(order_id),
 book_id INT,
-FOREIGN KEY (book_id) REFERENCES  Books(book_id),
+order_date DATE,
 
-order_date DATE
+FOREIGN KEY (order_id) REFERENCES  Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES  Books(book_id)
+
+
 );
 
 
