707efe257d6514b35b7bbdd67aba8be1f0619a0e
Herbert Asante <herbertasante66@gmail.com>
2026-01-17 16:56:55 +0000

    'alx_book_store' created

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,28 @@
-def calculate_price(price, tax):
-    total = price + (price * tax)
-    return total
+import mysql.connector
+from mysql.connector import Error
 
 
-print(calculate_price(100, 0.05))
+connection = None
+cursor = None
+
+try:
+    connection = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = 'admin'
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print ("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error  as e :
+    print(f"Error connecting to MYSQL: {e}")
+
+finally:
+    if cursor:
+        cursor.close()
+    if connection and connection.is_connected():
+        connection.close()
